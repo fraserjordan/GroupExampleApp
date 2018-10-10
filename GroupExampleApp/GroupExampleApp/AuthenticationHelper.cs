d66ae200d7c4ebfe3f44407bf45d082497a186e1
Fraser Jordan <FraserJ@provoke.co.nz>
2018-10-11 10:05:21 +1300

    Initial Commit

---
+++ GroupExampleApp/GroupExampleApp/AuthenticationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace GroupExampleApp
+{
+    public class AuthenticationHelper
+    {
+        public async Task<string> GetGraphAccessToken(string resource = null)
+        {
+            var authenticationContext = new AuthenticationContext("https://login.windows.net/tenant.onmicrosoft.com/");
+            var authenticationResult = await authenticationContext.AcquireTokenAsync("https://graph.microsoft.com", GetClientCredential());
+            return authenticationResult.AccessToken;
+        }
+
+        public ClientCredential GetClientCredential()
+        {
+            return new ClientCredential("ClientID", "ClientSecret");
+        }
+    }
+}
---
+++ GroupExampleApp/GroupExampleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeDevPnP.Core.Framework.Graph;
+
+namespace GroupExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var authHelper = new AuthenticationHelper();
+            var accessToken = authHelper.GetGraphAccessToken().Result;
+
+            var groupSettings = new
+            {
+                AllowExternalSenders = false,
+                AutoSubscribeNewMembers = false
+            };
+
+            GraphHttpClient.MakePatchRequestForString(
+                $"{GraphHttpClient.MicrosoftGraphV1BaseUri}groups/0dd7fae1-53b3-4423-8032-25f66a70870c",
+                groupSettings, "application/json", accessToken
+            );
+        }
+    }
+}
